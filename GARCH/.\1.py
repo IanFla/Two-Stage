c4c919a1855d2c35aedaadd5828eae7d47b74362
IanFla <51277149+IanFla@users.noreply.github.com>
2021-06-16 01:50:39 +0800

    bmb

---
+++ GARCH/.\1.py
@@ -0,0 +1,10 @@
+
+begin=dt.now()
+# pool=multiprocessing.Pool(8)
+# for d in D:
+#     for alpha in Alpha:
+#         pool.apply_async(func=experiment,args=(d,alpha))
+
+# pool.close()
+# pool.join()
+end=dt.now()
