3397710db91254576b9ba19e7955f75ee36a5288
IanFla <51277149+IanFla@users.noreply.github.com>
2021-07-17 16:22:31 +0800

    Create Exp1.py

---
+++ paper/Exp1.py
@@ -0,0 +1,15 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+class ABC:
+    def __init__(self):
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
