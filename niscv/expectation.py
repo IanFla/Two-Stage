0d35edadd06d30a22d51a8b35854846207503269
IanFla <51277149+IanFla@users.noreply.github.com>
2021-10-27 18:47:02 +0800

    abc

--- niscv/expectation.py
+++ niscv/expectation.py
@@ -2,15 +2,15 @@ import numpy as np
 from matplotlib import pyplot as plt
 from particles import resampling as rs
 from kde import KDE
-# import sklearn.linear_model as lmd
-# import scipy.optimize as opt
+import sklearn.linear_model as lm
+from datetime import datetime as dt
+import scipy.optimize as opt
 
 import scipy.stats as st
-# from datetime import datetime as dt
 # import pickle
 # import multiprocessing
-# import warnings
-# warnings.filterwarnings("ignore")
+import warnings
+warnings.filterwarnings("ignore")
 
 
 class Expectation:
@@ -45,9 +45,11 @@ class Expectation:
         self.weights_ = None
 
         self.controls_ = None
-        self.regO = None
-        self.regR = None
-        self.regL = None
+        self.reg = None
+        self.reg1 = None
+        self.reg2 = None
+        self.mu = None
+        self.avar = None
 
     def disp(self, text):
         if self.show:
@@ -60,17 +62,33 @@ class Expectation:
         q[q == 0] = 1
         return p / q
 
-    def __estimate(self, weights, funs, name, asymp=True):
-        mu = np.sum(weights * funs) / np.sum(weights) if self.sn else np.mean(weights * funs)
-        self.result.append(mu)
-        if asymp:
+    def __estimate(self, weights, funs, name, mode='normal'):
+        if mode == 'normal':
+            mu = np.sum(weights * funs) / np.sum(weights) if self.sn else np.mean(weights * funs)
+            self.result.append(mu)
             avar = np.mean((weights * (funs - mu) / np.mean(weights)) ** 2) if self.sn else np.var(weights * funs)
             self.result.append(avar)
             aerr = np.sqrt(avar / weights.size)
             self.disp('{} est: {:.4f}; a-var: {:.4f}; 95% C.I.: [{:.4f}, {:.4f}]'
                       .format(name, mu, avar, mu - 1.96 * aerr, mu + 1.96 * aerr))
+        elif mode == 'regression':
+            X = (self.__divi(self.controls_, self.proposal_)).T
+            self.mu = np.sum(weights * funs - X.dot(self.reg1.coef_)) / np.sum(weights - X.dot(self.reg2.coef_)) \
+                if self.sn else np.mean(weights * funs - X.dot(self.reg.coef_))
+            self.result.append(self.mu)
+            self.avar = np.mean(((weights * (funs - self.mu) - X.dot(self.reg1.coef_)) /
+                            np.mean(weights - X.dot(self.reg2.coef_))) ** 2) \
+                if self.sn else np.var(weights * funs - X.dot(self.reg.coef_))
+            self.result.append(self.avar)
+            aerr = np.sqrt(self.avar / weights.size)
+            self.disp('{} est: {:.4f}; a-var: {:.4f}; 95% C.I.: [{:.4f}, {:.4f}]'
+                      .format(name, self.mu, self.avar, self.mu - 1.96 * aerr, self.mu + 1.96 * aerr))
         else:
-            self.disp('{} est: {:.4f}'.format(name, mu))
+            mu = np.sum(weights * funs) / np.sum(weights) if self.sn else np.mean(weights * funs)
+            self.result.append(mu)
+            aerr = np.sqrt(self.avar / weights.size)
+            self.disp('{} est: {:.4f}; 95% C.I.: [{:.4f}, {:.4f}]'
+                      .format(name, mu,  self.mu - 1.96 * aerr, self.mu + 1.96 * aerr))
 
     def initial_estimation(self, size_kn, ratio, resample=True):
         size_est = ratio * size_kn
@@ -134,6 +152,83 @@ class Expectation:
         self.weights_ = self.__divi(self.target_, self.proposal_)
         self.__estimate(self.weights_, self.fun_, 'MIS')
 
+    def regression_estimation(self):
+        self.controls_ = self.controls(self.samples_)
+        X = (self.__divi(self.controls_, self.proposal_)).T
+        w = self.weights_
+        y = w * self.fun(self.samples_)
+
+        if self.sn:
+            self.reg1 = lm.LinearRegression().fit(X, y)
+            self.reg2 = lm.LinearRegression().fit(X, w)
+            self.disp('Regression R2: {:.4f} / {:.4f}'.format(self.reg1.score(X, y), self.reg2.score(X, w)))
+            self.result.extend([self.reg1.score(X, y), self.reg2.score(X, w)])
+        else:
+            self.reg = lm.LinearRegression().fit(X, y)
+            self.disp('Regression R2: {:.4f}'.format(self.reg.score(X, y)))
+            self.result.append(self.reg.score(X, y))
+
+        self.__estimate(self.weights_, self.fun_, 'RIS', mode='regression')
+
+    def likelihood_estimation(self, optimize=True, NR=True):
+        target = lambda zeta: -np.mean(np.log(self.proposal_ + zeta.dot(self.controls_)))
+        gradient = lambda zeta: -np.mean(self.__divi(self.controls_, self.proposal_ + zeta.dot(self.controls_)), axis=1)
+        hessian = lambda zeta: self.__divi(self.controls_, (self.proposal_ + zeta.dot(self.controls_))
+                                           ** 2).dot(self.controls_.T) / self.controls_.shape[1]
+        zeta0 = np.zeros(self.controls_.shape[0])
+        grad0 = gradient(zeta0)
+        self.disp('')
+        self.disp('MLE reference:')
+        self.disp('Origin: value: {:.4f}; grad: (min {:.4f}, mean {:.4f}, max {:.4f}, std {:.4f})'
+                  .format(target(zeta0), grad0.min(), grad0.mean(), grad0.max(), grad0.std()))
+
+        self.disp('')
+        self.disp('Theoretical results:')
+        X = (self.__divi(self.controls_, self.proposal_)).T
+        zeta1 = np.linalg.solve(np.cov(X.T, bias=True), X.mean(axis=0))
+        self.disp('MLE(The) zeta: (min {:.4f}, mean {:.4f}, max {:.4f}, std {:.4f}, norm {:.4f})'
+                  .format(zeta1.min(), zeta1.mean(), zeta1.max(), zeta1.std(), np.sqrt(np.sum(zeta1 ** 2))))
+        grad1 = gradient(zeta1)
+        self.disp('Theory: value: {:.4f}; grad: (min {:.4f}, mean {:.4f}, max {:.4f}, std {:.4f})'
+                  .format(target(zeta1), grad1.min(), grad1.mean(), grad1.max(), grad1.std()))
+        weights = self.weights_ * (1 - (X - X.mean(axis=0)).dot(zeta1))
+        self.disp('Reg weights: (min {:.4f}, mean {:.4f}, max {:.4f}, [<0] {}/{})'
+                  .format(weights.min(), weights.mean(), weights.max(), np.sum(weights < 0), weights.size))
+        self.__estimate(weights, self.fun_, 'RIS(The)', mode='likelihood')
+        weights = self.__divi(self.target_, self.proposal_ + zeta1.dot(self.controls_))
+        self.disp('MLE weights (The): (min {:.4f}, mean {:.4f}, max {:.4f}, [<0] {}/{})'
+                  .format(weights.min(), weights.mean(), weights.max(), np.sum(weights < 0), weights.size))
+        self.__estimate(weights, self.fun_, 'MLE(The)', mode='likelihood')
+
+        if optimize:
+            zeta2 = zeta0 if np.isnan(target(zeta1)) else zeta1
+            begin = dt.now()
+            if NR:
+                res = opt.root(lambda zeta: (gradient(zeta), hessian(zeta)), zeta2, method='lm', jac=True)
+            else:
+                cons = ({'type': 'ineq', 'fun': lambda zeta: self.proposal_ + zeta.dot(self.controls_),
+                         'jac': lambda zeta: self.controls_.T})
+                res = opt.minimize(target, zeta2, method='SLSQP', jac=gradient, constraints=cons,
+                                   options={'ftol': 1e-8, 'maxiter': 1000})
+
+            end = dt.now()
+            self.disp('')
+            self.disp('Optimization results (spent {} seconds):'.format((end - begin).seconds))
+            if res['success']:
+                zeta2 = res['x']
+                self.disp('MLE(Opt) zeta: (min {:.4f}, mean {:.4f}, max {:.4f}, std {:.4f}, norm {:.4f})'
+                          .format(zeta2.min(), zeta2.mean(), zeta2.max(), zeta2.std(), np.sqrt(np.sum(zeta2 ** 2))))
+                self.disp('Dist(zeta(Opt),zeta(The))={:.4f}'.format(np.sqrt(np.sum((zeta2 - zeta1) ** 2))))
+                grad2 = gradient(zeta2)
+                self.disp('Optimal: value: {:.4f}; grad: (min {:.4f}, mean {:.4f}, max {:.4f}, std {:.4f})'
+                          .format(target(zeta2), grad2.min(), grad2.mean(), grad2.max(), grad2.std()))
+                weights = self.__divi(self.target_, self.proposal_ + zeta2.dot(self.controls_))
+                self.disp('MLE weights (Opt): (min {:.4f}, mean {:.4f}, max {:.4f}, [<0] {}/{})'
+                          .format(weights.min(), weights.mean(), weights.max(), np.sum(weights < 0), weights.size))
+                self.__estimate(weights, self.fun_, 'MLE(Opt)', mode='likelihood')
+            else:
+                self.disp('MLE fail')
+
     def draw(self, grid_x, name, d=0):
         grid_X = np.zeros([grid_x.size, self.dim])
         grid_X[:, d] = grid_x
@@ -151,6 +246,11 @@ class Expectation:
             ax.plot(grid_x, opt_proposal.max() * nonpar_proposal / nonpar_proposal.max())
             ax.plot(grid_x, opt_proposal.max() * mix_proposal / mix_proposal.max())
             ax.legend(['optimal proposal', 'nonparametric proposal', 'mixture proposal'])
+        elif name == 'regression':
+            reg_proposal = (self.reg1.coef_ - self.mu * self.reg2.coef_).dot(self.controls(grid_X)) \
+                if self.sn else self.reg.coef_.dot(self.controls(grid_X)) + self.mu * self.mix_proposal(grid_X)
+            ax.plot(grid_x, np.abs(reg_proposal))
+            ax.legend(['optimal proposal', 'regression proposal'])
         else:
             print('name err! ')
 
@@ -160,7 +260,7 @@ class Expectation:
 
 def experiment(dim, size_est, sn, size_kn, ratio):
     mean = np.zeros(dim)
-    target = st.multivariate_normal(mean=mean).pdf
+    target = lambda x: 2 * st.multivariate_normal(mean=mean).pdf(x)
     fun = lambda x: x[:, 0]
     init_proposal = st.multivariate_normal(mean=mean, cov=4)
     grid_x = np.linspace(-5, 5, 200)
@@ -170,10 +270,13 @@ def experiment(dim, size_est, sn, size_kn, ratio):
     exp.density_estimation(bw=1.0, factor='scott', local=False, gamma=0.3, df=0, alpha0=0.1)
     exp.nonparametric_estimation()
     exp.draw(grid_x, name='nonparametric')
+    exp.regression_estimation()
+    exp.draw(grid_x, name='regression')
+    exp.likelihood_estimation(optimize=True, NR=True)
 
 
 def main():
-    experiment(dim=2, size_est=10000, sn=True, size_kn=1000, ratio=20)
+    experiment(dim=7, size_est=10000, sn=True, size_kn=1000, ratio=20)
 
 
 if __name__ == '__main__':
