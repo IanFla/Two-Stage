a5ecac81cabe13d745d6ffc15667026d3983f833
IanFla <51277149+IanFla@users.noreply.github.com>
2021-11-09 13:15:09 +0800

    rd

--- niscv/analysis/normal case.py
+++ niscv/analysis/normal case.py
@@ -2,10 +2,11 @@ import numpy as np
 from matplotlib import pyplot as plt
 import pickle
 import scipy.stats as st
+import sklearn.linear_model as lm
 
 
 def read(dim):
-    file = open('/Users/ianfla/Documents/GitHub/Two-Stage/niscv/data/normal5_' + str(dim) + 'D', 'rb')
+    file = open('/Users/ianfla/Documents/GitHub/Two-Stage/niscv/data/normal/normal6_' + str(dim) + 'D', 'rb')
     data = pickle.load(file)
     data = np.array([da[0] for da in data])
     return data
@@ -14,16 +15,18 @@ def read(dim):
 def plot(data, ax, label, c, mode='a-var', truth=None, n=''):
     size_kns = np.array([50, 100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600])
     if mode == 'nmse':
-        n = 500 * size_kns if n == 'IS' else 5000
+        n = 1000 * size_kns if n == 'IS' else 5000
         nMSE = n * np.mean((data - truth) ** 2, axis=0)
         ax.loglog(size_kns, nMSE, c, label=label)
     elif mode == 'nvar':
-        n = 500 * size_kns if n == 'IS' else 5000
+        n = 1000 * size_kns if n == 'IS' else 5000
         nvar = n * np.var(data, axis=0)
         ax.loglog(size_kns, nvar, c + '.', label=label)
     elif mode == 'a-var':
         avar_mean = data.mean(axis=0)
         ax.loglog(size_kns, avar_mean, c + '--', label=label)
+        fit = lm.LinearRegression().fit(np.log(size_kns.reshape([-1, 1])), np.log(avar_mean))
+        return fit.coef_[0]
     else:
         print('mode error! ')
 
@@ -34,6 +37,7 @@ def draw(dim, order, sn, ax):
     name = str(dim) + 'D, M' + str(order) + ', SN(' + str(sn) + ')'
     truth = st.norm.moment(order) + 1
 
+    result = []
     plot(data[:, index, :, 0], ax, label='IS nMSE', c='b', mode='nmse', truth=truth, n='IS')
     plot(data[:, index, :, 0], ax, label='IS nVAR', c='b', mode='nvar', truth=truth, n='IS')
     plot(data[:, index, :, 1], ax, label='IS mean(a-var)', c='b', mode='a-var', truth=None)
@@ -42,28 +46,35 @@ def draw(dim, order, sn, ax):
     plot(data[:, index, :, 3], ax, label='NIS mean(a-var)', c='y', mode='a-var', truth=None)
     plot(data[:, index, :, 4], ax, label='MIS nMSE', c='c', mode='nmse', truth=truth)
     plot(data[:, index, :, 4], ax, label='MIS nVAR', c='c', mode='nvar', truth=truth)
-    plot(data[:, index, :, 5], ax, label='MIS mean(a-var)', c='c', mode='a-var', truth=None)
+    result.append(plot(data[:, index, :, 5], ax, label='MIS mean(a-var)', c='c', mode='a-var', truth=None))
     plot(data[:, index, :, 6], ax, label='RIS nMSE', c='r', mode='nmse', truth=truth)
     plot(data[:, index, :, 6], ax, label='RIS nVAR', c='r', mode='nvar', truth=truth)
-    plot(data[:, index, :, 7], ax, label='RIS mean(a-var)', c='r', mode='a-var', truth=None)
+    result.append(plot(data[:, index, :, 7], ax, label='RIS mean(a-var)', c='r', mode='a-var', truth=None))
     plot(data[:, index, :, 8], ax, label='MLE nMSE', c='k', mode='nmse', truth=truth)
     plot(data[:, index, :, 8], ax, label='MLE nVAR', c='k', mode='nvar', truth=truth)
-    # ax.set_xlabel('log(kernel number)')
-    ax.legend()
+    ax.set_xlabel('log(kernel number)')
+    ax.set_ylim([10**(dim / 3.3 - 3), 10**(dim / 7 + 1.2)])
+    ax.legend(loc=3)
     ax.set_title(name)
+
+    return result
 
 
 def main(dim, ax):
-    settings = [[0, False], [1, False], [1, True], [2, False], [2, True], [3, False], [3, True], [4, False], [4, True]]
+    settings = [[0, False], [1, False], [1, True], [2, False], [2, True]]
+    result = []
     for j, setting in enumerate(settings):
-        draw(dim, order=setting[0], sn=setting[1], ax=ax[j])
+        result.append(draw(dim, order=setting[0], sn=setting[1], ax=ax[j]))
+
+    return result
 
 
 if __name__ == '__main__':
     plt.style.use('ggplot')
-    dims = [5, 7, 9]
-    fig, axs = plt.subplots(9, 3, figsize=[30, 50])
+    dims = [4, 6, 8]
+    R = []
+    fig, axs = plt.subplots(5, 3, figsize=[30, 30])
     for i, d in enumerate(dims):
-        main(d, axs[:, i])
+        R.append(main(d, axs[:, i]))
 
     fig.show()
---
+++ niscv/experiments/optimal proposal.py
@@ -0,0 +1,55 @@
+import numpy as np
+import scipy.stats as st
+from niscv.basic.expectation import Expectation
+from matplotlib import pyplot as plt
+
+
+def experiment(dim, order, size_est, sn, size_kn, ratio, ax):
+    results = []
+    mean = np.zeros(dim)
+    target = lambda x: st.multivariate_normal(mean=mean).pdf(x)
+    fun = lambda x: x[:, 0] ** order + 1
+    init_proposal = st.multivariate_normal(mean=mean + 0.5, cov=4)
+    grid_x = np.linspace(-5, 5, 200)
+    grid_X = np.zeros([grid_x.size, dim])
+    grid_X[:, 0] = grid_x
+
+    exp = Expectation(dim, target, fun, init_proposal, size_est, sn=sn, show=True)
+    exp.initial_estimation(size_kn, ratio, resample=True)
+    opt = exp.opt_proposal(grid_X)
+    init = exp.init_proposal(grid_X)
+    ax.plot(grid_x, opt, c='k', label='optimal proposal')
+    ax.plot(grid_x, opt.max() * init / init.max(), c='b', label='initial proposal')
+    results.extend([exp.result[-5], exp.result[-4]])
+    exp.density_estimation(bw=1.0, factor='scott', local=False, gamma=0.3, df=0, alpha0=0.1)
+    exp.nonparametric_estimation()
+    nonpar = exp.nonpar_proposal(grid_X)
+    mix = exp.mix_proposal(grid_X)
+    ax.plot(grid_x, opt.max() * nonpar / nonpar.max(), c='y', label='nonparametric proposal')
+    ax.plot(grid_x, opt.max() * mix / mix.max(), c='c', label='mixture proposal')
+    results.extend([exp.result[-4], exp.result[-3], exp.result[-2], exp.result[-1]])
+    exp.regression_estimation()
+    reg = (exp.reg1.coef_ - exp.mu * exp.reg2.coef_).dot(exp.controls(grid_X)) \
+        if exp.sn else exp.reg.coef_.dot(exp.controls(grid_X)) + exp.mu * exp.mix_proposal(grid_X)
+    ax.plot(grid_x, np.abs(reg), c='r', label='regression proposal')
+    results.extend([exp.result[-2], exp.result[-1]])
+    exp.likelihood_estimation()
+    results.append(exp.result[-1])
+    return results,  exp.result
+
+
+def main():
+    np.random.seed(19971107)
+    settings = [[0, False], [1, False], [1, True], [2, False], [2, True]]
+    plt.style.use('ggplot')
+    for i, setting in enumerate(settings):
+        fig, ax = plt.subplots(figsize=[8, 6])
+        experiment(dim=4, order=setting[0], size_est=5000, sn=setting[1], size_kn=300, ratio=1000, ax=ax)
+        ax.legend()
+        name = '4D, M' + str(setting[0]) + ', SN(' + str(setting[1]) + ')'
+        ax.set_title(name)
+        fig.show()
+
+
+if __name__ == '__main__':
+    main()
