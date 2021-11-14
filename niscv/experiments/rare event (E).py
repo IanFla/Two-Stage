8df4770d7758bc33d1ea9493cb1e76a3a67cc71a
IanFla <51277149+IanFla@users.noreply.github.com>
2021-11-14 20:38:00 +0800

    Update rare event (E).py

--- niscv/experiments/rare event (E).py
+++ niscv/experiments/rare event (E).py
@@ -83,6 +83,7 @@ def main(b):
 
 if __name__ == '__main__':
     # main(2.0)
+    np.random.seed(19971107)
     R1 = experiment(dim=5, b=2.0, size_est=10000, show=False, size_kn=500, ratio=1000,
                     resample=False, auto=True, k=1)
     R2 = experiment(dim=5, b=2.0, size_est=10000, show=False, size_kn=500, ratio=1000,
