6c8ddcba3f58480543f5ed88eb3c0edb74b7bcdc
IanFla <51277149+IanFla@users.noreply.github.com>
2021-11-14 09:30:14 +0800

    Update rare event (E).py

--- niscv/experiments/rare event (E).py
+++ niscv/experiments/rare event (E).py
@@ -82,16 +82,4 @@ def main(b):
 
 
 if __name__ == '__main__':
-    # main(2.0)
-    experiment(dim=5, b=2, size_est=10000, show=True, size_kn=500, ratio=1000, resample=False,
-               auto=True, k=1)
-    experiment(dim=5, b=2, size_est=10000, show=True, size_kn=500, ratio=5,
-               resample=True, auto=True, k=1)
-    experiment(dim=5, b=2, size_est=10000, show=True, size_kn=500, ratio=5,
-               resample=True, auto=False, k=None)
-    experiment(dim=5, b=2, size_est=10000, show=True, size_kn=500, ratio=5,
-               resample=True, auto=True, k=2)
-    experiment(dim=5, b=2, size_est=10000, show=True, size_kn=500, ratio=5,
-               resample=True, auto=True, k=3)
-    experiment(dim=5, b=2, size_est=10000, show=True, size_kn=500, ratio=5,
-               resample=True, auto=True, k=4)
+    main(2.0)
