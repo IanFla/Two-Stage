8612fe5c5bd0f7dd396152b2522df32bfa4f125e
r7user5 <ianzhang@connect.hku.hk>
2021-11-18 12:52:34 +0800

    server

--- niscv/experiments/garch (E).py
+++ niscv/experiments/garch (E).py
@@ -44,8 +44,8 @@ def experiment(d, alpha, size_est, show, size_kn, ratio, bw, km, local, gamma, a
 
 
 def main():
-    experiment(d=1, alpha=0.05, size_est=100000, show=True,
-               size_kn=1500, ratio=1000, bw=1.1, km=2, local=True, gamma=0.3, alpha0=0.1, server=False)
+    experiment(d=2, alpha=0.05, size_est=100000, show=True,
+               size_kn=2000, ratio=1000, bw=1.3, km=2, local=True, gamma=0.3, alpha0=0.1, server=True)
 
 
 if __name__ == '__main__':
