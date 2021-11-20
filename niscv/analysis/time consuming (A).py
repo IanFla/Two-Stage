c70be8e269c7104da694803928d02d25d2b3b9f6
IanFla <51277149+IanFla@users.noreply.github.com>
2021-11-20 10:39:55 +0800

    nb

--- niscv/analysis/time consuming (A).py
+++ niscv/analysis/time consuming (A).py
@@ -31,7 +31,7 @@ def plot(data, ax, labels, c):
     size_ests = np.array([1000, 2000, 3000, 5000, 7000, 10000, 20000, 30000, 50000, 70000, 100000])
     lines = ['-', '--', '-.', ':', '.']
     for i, dat in enumerate(data):
-        ax.loglog(size_ests, dat, c + lines[i], label=labels[i])
+        ax.plot(size_ests, dat, c + lines[i], label=labels[i])
 
     data = data.mean(axis=0)
     fit = lm.LinearRegression().fit(np.log(size_ests.reshape([-1, 1])), np.log(data))
@@ -43,12 +43,12 @@ def main(dim, ax, name):
     time = times.sum(axis=0)
     result = [plot(time[:, :, 0], ax, labels=['IS+re 1', 'IS+re 1', 'IS+re 1', 'IS+re 1', 'IS+re 1'], c='b'),
               plot(time[:, :, 1], ax, labels=['NIS+MIS 1', 'NIS+MIS 2', 'NIS+MIS 3', 'NIS+MIS 4', 'NIS+MIS 5'], c='y'),
-              plot(time[:, :, 2], ax, labels=['RIS 1', 'RIS 2', 'RIS 3', 'RIS 4', 'RIS 5'], c='c'),
-              plot(time[:, :, 3], ax, labels=['MLE 1', 'MLE 2', 'MLE 3', 'MLE 4', 'MLE 5'], c='r')]
-    ax.legend()
-    ax.set_xlabel('log(sample size)')
-    ax.set_ylabel('log(time - seconds)')
-    ax.set_ylim([1, 100])
+              plot(time[:, :, 2], ax, labels=['RIS 1', 'RIS 2', 'RIS 3', 'RIS 4', 'RIS 5'], c='r'),
+              plot(time[:, :, 3], ax, labels=['MLE 1', 'MLE 2', 'MLE 3', 'MLE 4', 'MLE 5'], c='k')]
+    ax.legend(loc=1)
+    ax.set_xlabel('sample size')
+    ax.set_ylabel('time - seconds')
+    ax.set_ylim([0, 100])
     ax.set_title(name)
     return result
 
