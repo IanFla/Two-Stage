d6666574f908485e4d364d7f298f049f2c6fee4f
IanFla <51277149+IanFla@users.noreply.github.com>
2021-11-22 22:12:11 +0800

    Update garch 2 (A).py

--- niscv/analysis/garch 2 (A).py
+++ niscv/analysis/garch 2 (A).py
@@ -10,7 +10,7 @@ def read(num):
 
 
 def main():
-    data = read(1)
+    data = np.vstack([read(num) for num in np.arange(1, 3)])
     means = data.mean(axis=0)
     nvars = data.var(axis=0)
     nvars[:, 0] = 2000 * 3000 * nvars[:, 0]
